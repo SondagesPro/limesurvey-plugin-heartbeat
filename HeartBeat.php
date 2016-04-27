ac07e49a8ace456d50fefca4940576b9f90741df
PRIJCK Frederik (FPRJ) <frederik.prijck@gmail.com>
2016-04-27 16:44:46 +0200

    Format code

--- HeartBeat.php
+++ HeartBeat.php
@@ -1,12 +1,11 @@
 <?php
 
 class HeartBeat extends PluginBase {
-
-    protected $storage = 'DbStorage';
-
-    static protected $description = 'Implement a heartbeat to ensure the session is kept alive.';
+    
     static protected $name = 'HeartBeat';
-
+    static protected $description = 'Implement a heartbeat to ensure the session is kept alive.';
+    
+    protected $storage = 'DbStorage';
     protected $settings = array(
         'interval'=>array(
             'type'=>'int',
@@ -14,7 +13,6 @@ class HeartBeat extends PluginBase {
             'default' => 10000
         )
     );
-
 
     public function __construct(PluginManager $manager, $id) {
         parent::__construct($manager, $id);
@@ -25,18 +23,19 @@ class HeartBeat extends PluginBase {
 
     public function beforeSurveyPage()
     {
-       // Get the js directory
-		$jsPath=Yii::app()->assetManager->publish(dirname(__FILE__) . '/js/');
-		// Register the js file
+        // Get the js directory
+	$jsPath=Yii::app()->assetManager->publish(dirname(__FILE__) . '/js/');
+	// Register the js file
         Yii::app()->clientScript->registerScriptFile($jsPath.'/heartbeat.js');
-		// Create the endpoint url
+	// Create the endpoint url
         $aOption['endpoint']=$this->api->createUrl('plugins/direct', array('plugin' => get_class($this),'function' => 'beat'));
         // Get the settings
         $interval = (int)$this->get('interval');
+        // Ensure the interval is not < 5000.
         $aOption['interval']= $interval < 5000 ? 5000 : $interval;
-		// Create the javascript code to inject in the page
+	// Create the javascript code to inject in the page
         $heartBeatScript="heartBeat.beat(".ls_json_encode($aOption).");";
-		// Inject js into the page
+	// Inject js into the page
         Yii::app()->clientScript->registerScript("heartbeat", $heartBeatScript, CClientScript::POS_END);
     }
 
@@ -55,8 +54,7 @@ class HeartBeat extends PluginBase {
 
     private function actionBeat()
     {
-		// Access the session to make sure it stays alive
+	// Access the session to make sure it stays alive
         $iSurveyId=Yii::app()->session['LEMsid'];
     }
-
 }
