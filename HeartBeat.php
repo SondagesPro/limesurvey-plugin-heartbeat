9af0099490f1a82ba517a0898c616ca9876b5da6
Denis Chenu <denis@sondages.pro>
2016-04-28 16:15:24 +0200

    [dev] Replace tab with space, use init and PSR (3.0 more compatible) [doc] Add min to settings directly

--- HeartBeat.php
+++ HeartBeat.php
@@ -1,22 +1,23 @@
 <?php
 
-class HeartBeat extends PluginBase {
-    
+class HeartBeat extends \ls\pluginmanager\PluginBase {
+
     static protected $name = 'HeartBeat';
     static protected $description = 'Implement a heartbeat to ensure the session is kept alive.';
-    
+
     protected $storage = 'DbStorage';
     protected $settings = array(
         'interval'=>array(
             'type'=>'int',
-            'label' => 'Interval of the heartbeat (min: 5000 ms)',
-            'default' => 10000
+            'label' => 'Interval of the heartbeat (in ms, min: 5000 ms)',
+            'default' => 10000,
+            'htmlOptions'=>array(
+                'min'=>5000
+            )
         )
     );
 
-    public function __construct(PluginManager $manager, $id) {
-        parent::__construct($manager, $id);
-
+    public function init() {
         $this->subscribe('beforeSurveyPage');
         $this->subscribe('newDirectRequest');
     }
@@ -24,18 +25,18 @@ class HeartBeat extends PluginBase {
     public function beforeSurveyPage()
     {
         // Get the js directory
-	$jsPath=Yii::app()->assetManager->publish(dirname(__FILE__) . '/js/');
-	// Register the js file
+        $jsPath=Yii::app()->assetManager->publish(dirname(__FILE__) . '/js/');
+        // Register the js file
         Yii::app()->clientScript->registerScriptFile($jsPath.'/heartbeat.js');
-	// Create the endpoint url
+        // Create the endpoint url
         $aOption['endpoint']=$this->api->createUrl('plugins/direct', array('plugin' => get_class($this),'function' => 'beat'));
         // Get the settings
         $interval = (int)$this->get('interval');
         // Ensure the interval is not < 5000.
         $aOption['interval']= $interval < 5000 ? 5000 : $interval;
-	// Create the javascript code to inject in the page
+        // Create the javascript code to inject in the page
         $heartBeatScript="heartBeat.beat(".ls_json_encode($aOption).");";
-	// Inject js into the page
+        // Inject js into the page
         Yii::app()->clientScript->registerScript("heartbeat", $heartBeatScript, CClientScript::POS_END);
     }
 
@@ -54,7 +55,7 @@ class HeartBeat extends PluginBase {
 
     private function actionBeat()
     {
-	// Access the session to make sure it stays alive
+        // Access the session to make sure it stays alive
         $iSurveyId=Yii::app()->session['LEMsid'];
     }
 }
