6a94ab95a71113e897ea08838587e3a700410794
Denis Chenu <denis@sondages.pro>
2016-04-28 17:58:47 +0200

    [feature] Default value is set according to session.gc_maxlifetime [feature] Show actual session.gc_maxlifetime in help part [feature] Show something in request if debug is set. Maybe interval set here ?

--- HeartBeat.php
+++ HeartBeat.php
@@ -9,8 +9,8 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
     protected $settings = array(
         'interval'=>array(
             'type'=>'int',
-            'label' => 'Interval of the heartbeat (in seconds, min: 5 s)',
-            'default' => 10,
+            'label' => 'Interval of the heartbeat (in seconds, minimum: 5s)',
+            'default' => 120,
             'htmlOptions'=>array(
                 'min'=>5
             )
@@ -30,8 +30,8 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
         Yii::app()->clientScript->registerScriptFile($jsPath.'/heartbeat.js');
         // Create the endpoint url
         $aOption['endpoint']=$this->api->createUrl('plugins/direct', array('plugin' => get_class($this),'function' => 'beat'));
-        // Get the settings
-        $interval = (int)$this->get('interval');
+        // Get the settings : we are in survey, then need nul,null for default value
+        $interval = (int)$this->get('interval',null,null,self::getDefaultInterval());
         // Ensure the interval is not < 5 and convert to ms
         $aOption['interval']= ($interval < 5 ? 5 : $interval) * 1000;
         // Create the javascript code to inject in the page
@@ -52,10 +52,35 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
                 throw new CHttpException(404,'Unknow action');
         }
     }
+    /*
+     * Set default when show setting
+     */
+    public function getPluginSettings($getValues=true)
+    {
+        $this->settings['interval']['default']=self::getDefaultInterval();
+        $this->settings['interval']['help']="Your actual session.gc_maxlifetime is ".App()->session->getTimeout()."s";
+        return parent::getPluginSettings($getValues);
 
+    }
+
+    /**
+     * Default value for interval
+     */
+    private function getDefaultInterval()
+    {
+        return intval(App()->session->getTimeout()*0.9); // Set default to 90% of timeout
+    }
+    /**
+     * Action called in ajax to reset session time out
+     */
     private function actionBeat()
     {
         // Access the session to make sure it stays alive
         $iSurveyId=Yii::app()->session['LEMsid'];
+        // If LS debug is set : show the survey id
+        if(App()->getConfig('debug'))
+        {
+            echo $iSurveyId;
+        }
     }
 }
