4b1f8b97c5cab9a9e6a0859edc9f1a1ba18e5efa
PRIJCK Frederik (FPRJ) <frederik.prijck@gmail.com>
2016-04-29 09:55:17 +0200

    Basic solution for #5

--- HeartBeat.php
+++ HeartBeat.php
@@ -14,6 +14,11 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
             'htmlOptions'=>array(
                 'min'=>5
             )
+        ),
+        'useSessionLifeTime'=>array(
+            'type'=>'checkbox',
+            'label' => 'Use 90% of the session lifetime as interval',
+            'default' => true
         )
     );
 
@@ -30,8 +35,9 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
         Yii::app()->clientScript->registerScriptFile($jsPath.'/heartbeat.js');
         // Create the endpoint url
         $aOption['endpoint']=$this->api->createUrl('plugins/direct', array('plugin' => get_class($this),'function' => 'beat'));
-        // Get the settings : we are in survey, then need nul,null for default value
-        $interval = (int)$this->get('interval',null,null,self::getDefaultInterval());
+        
+        // Get the settings : we are in survey, then need null, null for default value
+        $interval = (int)$this->getInterval();
         // Ensure the interval is not < 5 and convert to ms
         $aOption['interval']= ($interval < 5 ? 5 : $interval) * 1000;
         // Create the javascript code to inject in the page
@@ -52,15 +58,30 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
                 throw new CHttpException(404,'Unknow action');
         }
     }
+    
     /*
      * Set default when show setting
      */
     public function getPluginSettings($getValues=true)
     {
-        $this->settings['interval']['default']=self::getDefaultInterval();
-        $this->settings['interval']['help']="Your actual session.gc_maxlifetime is ".App()->session->getTimeout()."s";
-        return parent::getPluginSettings($getValues);
+        $aPluginSettings = parent::getPluginSettings($getValues);
 
+        $useSessionLifeTime = (boolean) $this->get('useSessionLifeTime', null, null, false);
+
+        // If it is a post request, it could be an autosubmit so read posted
+        // value over the saved value
+        if (App()->request->isPostRequest) {
+            $useSessionLifeTime = App()->request->getPost('useSessionLifeTime', $useSessionLifeTime);
+        }
+        
+        if($useSessionLifeTime == true){
+            unset($aPluginSettings['interval']);
+        }else{
+            $aPluginSettings['interval']['default']=self::getDefaultInterval();
+            $aPluginSettings['interval']['help']="Your actual session.gc_maxlifetime is ".App()->session->getTimeout()."s";
+        }
+        
+        return $aPluginSettings;
     }
 
     /**
@@ -70,6 +91,23 @@ class HeartBeat extends \ls\pluginmanager\PluginBase {
     {
         return intval(App()->session->getTimeout()*0.9); // Set default to 90% of timeout
     }
+    
+    /*
+    * Get the interval to be used by the heartbeat
+    */
+    private function getInterval(){
+        // Get the boolean indicating to use the session life time or not
+        $useSessionLifeTime = (boolean)$this->get('useSessionLifeTime', null, null, false);
+        
+        if($useSessionLifeTime == true){
+            return getDefaultInterval();
+        }else{
+            return (int)$this->get('interval', null, null, self::getDefaultInterval());
+        }
+        
+        return interval;
+    }
+    
     /**
      * Action called in ajax to reset session time out
      */
